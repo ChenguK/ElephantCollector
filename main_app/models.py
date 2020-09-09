b3d7e7cdebe780b9449c55ace447752e78ef0075
ChenguK <ChenguKargbo@gmail.com>
2020-09-08 23:51:34 -0400

    populating info from api

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+
+class Elephant(models.Model): 
+    name = models.CharField(max_length=100)
+    affiliation = models.CharField(max_length=200)
+    species = models.CharField(max_length=100)
+    sex = models.CharField(max_length=50)
+    fictional = models.CharField(max_length=50)
+    dob = models.CharField(max_length=50)
+    dod = models.CharField(max_length=50)
+    wikilink = models.URLField(max_length=100)
+    image = models.ImageField(width_field=100)
+    note = models.TextField(max_length=300)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Elephant
+import requests
+
+
+# class Elephant:
+#     def __init__(self, name, affiliation, species, sex, fictional, dob, dod, wikilink, image, note):
+#         self.name = name
+#         self.affiliation = affiliation
+#         self.species = species
+#         self.sex = sex
+#         self.fictional = fictional
+#         self.dob = dob
+#         self.dod = dod
+#         self.wikilink = wikilink
+#         self.image = image
+#         self.note = note
+
+# elephants = [
+#     Elephant('Abul-Abbas', 'Charlemagne', 'Asian', 'Male', 'false', 'Unavailable', '810', 'https://en.wikipedia.org/wiki/Abul-Abbas', 'https://elephant-api.herokuapp.com/pictures/001.jpg', 'An elephant given to Carolingian emperor Charlemagne by the Abbasid caliph Harun al-Rashid.')
+#     # Elephant('Balarama', 'Dasara', 'Asian', 'Male', 'false', '1958', ' - ', 'https://en.wikipedia.org/wiki/Balarama_(elephant)', 'https://elephant-api.herokuapp.com/pictures/missing.jpg', 'A lead elephant of the world-famous Mysore Dasara procession.')
+# ]
 
 # Create your views here.
 def home(request):
@@ -7,3 +28,8 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def elephants_index(request):
+    response = requests.get('https://elephant-api.herokuapp.com/elephants')
+    elephants = response.json()
+    return render(request, 'elephants/index.html', { 'elephants': elephants })
