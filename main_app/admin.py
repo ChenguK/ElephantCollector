2d49c60ffda31c9275669a234485058604cb6dd3
ChenguK <ChenguKargbo@gmail.com>
2020-09-24 16:36:03 -0400

    adding model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Elephant, Care, Photo
+from .models import Elephant, Care, Trainer, Photo
 
 # Register your models here.
 
 admin.site.register(Elephant)
 admin.site.register(Care)
+admin.site.register(Trainer)
 admin.site.register(Photo)
