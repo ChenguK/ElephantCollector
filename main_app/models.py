78489aaa63143a63476f1094dd9cc41bd153b196
ChenguK <ChenguKargbo@gmail.com>
2020-09-24 21:47:34 -0400

    saving

--- main_app/models.py
+++ main_app/models.py
@@ -27,8 +27,6 @@ SERVICES = (
 class Trainer(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
     def __str__(self):
         return self.name
