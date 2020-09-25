0e01614360aee6b772b67e9cadd5b746e061617c
ChenguK <ChenguKargbo@gmail.com>
2020-09-24 21:19:11 -0400

    new

--- main_app/views.py
+++ main_app/views.py
@@ -131,7 +131,7 @@ def trainers_detail(request, trainer_id):
 
 class TrainerCreate(LoginRequiredMixin, CreateView):
     model = Trainer
-    fields = ['name', 'color']
+    fields = '__all__'
 
 class TrainerUpdate(LoginRequiredMixin, UpdateView):
     model = Trainer
